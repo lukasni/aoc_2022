4d9d4900c4d754e3c91459d0499536d02c1eaafe
Lukas Niederberger <niederbergerlukas@gmail.com>
2022-12-04 22:44:41 -0800

    Day 4 complete

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,34 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> AdventOfCode.parse_lines()
+    |> Enum.map(&parse_pairs/1)
+    |> Enum.map(&subset?/1)
+    |> Enum.count(& &1)
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> AdventOfCode.parse_lines()
+    |> Enum.map(&parse_pairs/1)
+    |> Enum.map(&overlap?/1)
+    |> Enum.count(& &1)
+  end
+
+  defp parse_pairs(pair) do
+    [i1, e1, i2, e2] =
+      pair
+      |> String.split(["-", ","])
+      |> Enum.map(&String.to_integer/1)
+
+    {MapSet.new(i1..e1), MapSet.new(i2..e2)}
+  end
+
+  defp subset?({s1, s2}) do
+    MapSet.subset?(s1, s2) or MapSet.subset?(s2, s1)
+  end
+
+  defp overlap?({s1, s2}) do
+    not MapSet.disjoint?(s1, s2)
   end
 end
