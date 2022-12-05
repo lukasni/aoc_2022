c3a8c862f09a5b8b676b94bae24f369e337f10de
Lukas Niederberger <niederbergerlukas@gmail.com>
2022-12-04 21:46:32 -0800

    Day 2 complete

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,40 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  @score_map_1 %{
+    "A X" => (1+3),
+    "A Y" => (2+6),
+    "A Z" => (3+0),
+    "B X" => (1+0),
+    "B Y" => (2+3),
+    "B Z" => (3+6),
+    "C X" => (1+6),
+    "C Y" => (2+0),
+    "C Z" => (3+3)
+  }
+
+  @score_map_2 %{
+    "A X" => (3+0),
+    "A Y" => (1+3),
+    "A Z" => (2+6),
+    "B X" => (1+0),
+    "B Y" => (2+3),
+    "B Z" => (3+6),
+    "C X" => (2+0),
+    "C Y" => (3+3),
+    "C Z" => (1+6),
+  }
+  def part1(input) do
+    input
+    |> AdventOfCode.parse_lines()
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(&Map.get(@score_map_1, &1))
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> AdventOfCode.parse_lines()
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(&Map.get(@score_map_2, &1))
+    |> Enum.sum()
   end
 end
